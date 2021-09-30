ccfa5cbbcbd53a67ae5b9a775997dfcbc97a44db
gloria2000 <gloria00thomas@gmail.com>
2021-09-30 23:57:51 +0530

    Connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,39 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:greenie/pages/feed.dart';
-void main() {
-  runApp(const MyApp());
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp();
+  runApp(MaterialApp(
+    title: 'Flutter Database Example',
+    home: MyApp(app: app),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
+class MyApp extends StatefulWidget {
+   MyApp({Key? key, required this.app}) : super(key: key);
+  final FirebaseApp app;
+  
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  //final referenceDatabase = FirebaseDatabase.instance;
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
